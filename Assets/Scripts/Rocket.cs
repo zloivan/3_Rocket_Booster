a3465a63c387988c76eaeecf5ae8f1b3951b7221
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-10 17:19:08 +0300

    Added a getKey methods for thrustrating and rotating. Print that in a console.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))//thrust the ship
+        {
+            print("Ship is thrustating.");
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Rotating left.");
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Rotating right.");
+        }
+        
+    }
+}
