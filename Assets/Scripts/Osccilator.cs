6d8dad8ec48f62ba182d92c3e2d8a8ff368613eb
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-11 21:47:26 +0300

    Modified moving obsticles script so it moved objects automaticly.

--- Assets/Scripts/Osccilator.cs
+++ Assets/Scripts/Osccilator.cs
@@ -8,12 +8,17 @@ public class Osccilator : MonoBehaviour {
     [Range(0, 1)]
     float MovingFactor;
 
+
+
     [SerializeField]
-    Vector3 MovingPosition;
+    Vector3 MovingPosition = new Vector3(10f,10f,10f);
+
 
     Vector3 startingPoint;
-	// Use this for initialization
-	void Start ()
+    [SerializeField] float period = 2f;
+
+    // Use this for initialization
+    void Start ()
     {
         startingPoint = transform.position;
 	}
@@ -21,8 +26,14 @@ public class Osccilator : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        float cicles = Time.time / period; //Grows constantly from 0
         Vector3 offset = MovingPosition * MovingFactor;
 
+        const float Tao = Mathf.PI * 2f; // about 6.28
+
+        float rawSinWave = Mathf.Sin(cicles * Tao);
+        MovingFactor = rawSinWave / 2f + 0.5f;
+        
         transform.position = startingPoint + offset;
 	}
 }
