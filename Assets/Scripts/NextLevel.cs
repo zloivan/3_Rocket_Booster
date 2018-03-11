0912bb97f6b3537206d8e3947ca83963efeffc9e
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-11 05:31:19 +0300

    Created sandbox, changed landing pad and made its own prefab.

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
