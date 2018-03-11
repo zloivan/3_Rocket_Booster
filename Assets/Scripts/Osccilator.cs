7510f15a6417bf1f5317784ede3c8f340a15cf1f
Иван Хомичков <ivan.khomichkov@gmail.com>
2018-03-11 20:59:39 +0300

    Added a manual way of moving obsticles. Added new prefab for moving obsticles.

---
+++ Assets/Scripts/Osccilator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Osccilator : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0, 1)]
+    float MovingFactor;
+
+    [SerializeField]
+    Vector3 MovingPosition;
+
+    Vector3 startingPoint;
+	// Use this for initialization
+	void Start ()
+    {
+        startingPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 offset = MovingPosition * MovingFactor;
+
+        transform.position = startingPoint + offset;
+	}
+}
